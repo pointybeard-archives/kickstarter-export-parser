87d0649d1a97ef60ac425fd37720a993f55c04f0
Alannah Kearney <hi@alannahkearney.com>
2019-04-05 10:14:21 +1100

    Updated unit tests for phpunit v8.

--- tests/BackerArchiveTest.php
+++ tests/BackerArchiveTest.php
@@ -1,59 +1,65 @@
 <?php
-namespace pointybeard\Kickstarter\ExportParser;
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
 use pointybeard\Kickstarter\ExportParser\Lib;
 use pointybeard\Kickstarter\ExportParser\Lib\Exceptions\ZipArchiveException;
 use pointybeard\Kickstarter\ExportParser\Tests\Seeders;
 
-class BackerArchiveTest extends \PHPUnit_Framework_TestCase
+class BackerArchiveTest extends TestCase
 {
-	/**
-	 * Simple test to load up a valid ZIP archive into the BackerArchive object
-	 */
-	public function testLoadValidArchiveValidCSV(){
-		$archive = new Lib\BackerArchive(__DIR__.'/archives/valid-csv.zip');
-		
-		$this->assertTrue($archive instanceof Lib\BackerArchive);
-		
-		return $archive;
-	}
-	
-	/**
-	 * This is a valid ZIP, but the contents are no CSV data
-     *
-     * @expectedException        pointybeard\Kickstarter\ExportParser\Lib\Exceptions\ZipArchiveException
-     * @expectedExceptionMessage Data does not appear to be valid CSV. Please check contents.
+    /**
+     * Simple test to load up a valid ZIP archive into the BackerArchive object
      */
-	public function testLoadValidArchiveNonCSV(){
-		$archive = new Lib\BackerArchive(__DIR__.'/archives/valid-noncsv.zip');
-	}
-	
-	/**
-	 * This is an invalid ZIP file. It will not load.
-     *
-     * @expectedException        pointybeard\Kickstarter\ExportParser\Lib\Exceptions\ZipArchiveException
-     * @expectedExceptionMessageRegExp #Could not open file `[^`]+`. Please check it is a valid Zip archive. Error Code: 19#
-     */
-	public function testLoadInvalidArchive(){
-		$archive = new Lib\BackerArchive(__DIR__.'/archives/invalid.zip');
-	}
+    public function testLoadValidArchiveValidCSV() : Lib\BackerArchive
+    {
+        $archive = new Lib\BackerArchive(__DIR__ . '/archives/valid-csv.zip');
 
-	/**
-	 * Try to load a zip file that does not exist.
-     *
-     * @expectedException        pointybeard\Kickstarter\ExportParser\Lib\Exceptions\ZipArchiveException
-     * @expectedExceptionMessageRegExp #Could not open file `[^`]+`. Please check it is a valid Zip archive. Error Code: 11#
-     */
-	public function testLoadNoArchive(){
-		$archive = new Lib\BackerArchive(__DIR__.'/archives/doesnotexist.zip');
-	}
+        $this->assertTrue($archive instanceof Lib\BackerArchive);
 
-	/**
-	 * @depends testLoadValidArchiveValidCSV
-	 */
-	public function testIterateOverRecords($archive){
-	    foreach ($archive->rewards() as $r) {
-			$this->assertTrue($r['records'] instanceof Lib\RecordIterator);
-	    }
-	    $archive->close();
-	}
+        return $archive;
+    }
+
+    /**
+     * This is a valid ZIP, but the contents are not CSV data
+     */
+    public function testLoadValidArchiveNonCSV() : void
+    {
+        $this->expectException(ZipArchiveException::class);
+        $this->expectExceptionMessage('Data does not appear to be valid CSV. Please check contents.');
+        new Lib\BackerArchive(__DIR__ . '/archives/valid-noncsv.zip');
+    }
+
+    /**
+     * This is an invalid ZIP file. It will not load.
+     *
+     */
+    public function testLoadInvalidArchive() : void
+    {
+        $this->expectException(ZipArchiveException::class);
+        $this->expectExceptionMessageRegExp('@Could not open file `[^`]+`. Please check it is a valid Zip archive. Error Code: \d+@');
+        new Lib\BackerArchive(__DIR__ . '/archives/invalid.zip');
+    }
+
+    /**
+     * Try to load a zip file that does not exist.
+     */
+    public function testLoadNoArchive() : void
+    {
+        $this->expectException(ZipArchiveException::class);
+        $this->expectExceptionMessageRegExp('@Could not open file `[^`]+`. Please check it is a valid Zip archive. Error Code: \d+@');
+        new Lib\BackerArchive(__DIR__ . '/archives/doesnotexist.zip');
+    }
+
+    /**
+     * @depends testLoadValidArchiveValidCSV
+     */
+    public function testIterateOverRecords($archive) : void
+    {
+        foreach ($archive->rewards() as $r) {
+            $this->assertTrue($r['records'] instanceof Lib\RecordIterator);
+        }
+        $archive->close();
+    }
 }
